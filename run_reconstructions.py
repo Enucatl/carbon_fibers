a92a978185661c9d901c95aa00190a896d3030ba
Matteo Abis <gmatteo..abis@gmail.com>
2013-12-10 13:27:57 +0100

    Added alu1mm (empty) dataset and logging module

--- run_reconstructions.py
+++ run_reconstructions.py
@@ -13,6 +13,11 @@ import numpy as np
 import os
 import re
 
+import logging
+import logging.config
+from dpc_reconstruction.logger_config import config_dictionary
+log = logging.getLogger()
+
 reconstruction_bin = "flats_every.py"
 from dpc_reconstruction.io.hdf5 import output_name
 
@@ -29,47 +34,60 @@ def extract_date(string):
 
 
 if __name__ == '__main__':
-    steps = 21
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("files", nargs='+',
                         help="files with the scans")
-    parser.add_argument("steps", nargs='?',
+    parser.add_argument("--steps", nargs='?',
                         type=int, default=21,
                         help="phase steps")
-    parser.add_argument("flats_every", nargs='?',
+    parser.add_argument("--flats_every", nargs='?',
                         type=int, default=5,
                         help="flats every n scans")
-    parser.add_argument("n_flats", nargs='?',
+    parser.add_argument("--n_flats", nargs='?',
                         type=int, default=5,
                         help="flats to average")
-    parser.add_argument("angle_delta", nargs='?',
+    parser.add_argument("--angle_delta", nargs='?',
                         type=float, default=4.5,
                         help="angular step")
+    parser.add_argument("--verbose", "-v",
+                        action="store_true",
+                        help="print debug info.")
     args = parser.parse_args()
+    if args.verbose:
+        config_dictionary['handlers']['default']['level'] = 'DEBUG'
+        config_dictionary['loggers']['']['level'] = 'DEBUG'
+    logging.config.dictConfig(config_dictionary)
 
+    # determine the length of each block
     files = args.files
     n = len(files) // (args.flats_every + args.n_flats)
 
+    # built the command for the reconstruction program
     command = "{0} ".format(reconstruction_bin)
     command += " ".join(args.files)
     command += " -o "
-    #command += " -v "
+    if args.verbose:
+        command += " -v "
     command += " -j 7 "
     command += " --flats_every {0} ".format(args.flats_every)
     command += " --n_flats {0} ".format(args.n_flats)
     command += " --steps {0} ".format(args.steps)
-    print(command)
+    log.debug(command)
     check_call(command, shell=True)
+
+    # open the hdf5 file with the reconstruction
     input_file_name = os.path.dirname(
         output_name(files, "MergeFlatsEvery")) + ".hdf5"
     h5_file = h5py.File(input_file_name, "r")
     big_dataset = h5_file['postprocessing']['MergeFlatsEvery']
     datasets = np.dsplit(big_dataset, n)
-    print(len(datasets))
     output_file_name = extract_date(input_file_name) + ".csv"
     angle_delta = args.angle_delta
+
+    # write the absorption and dark field of each pixel to the csv output
+    # file
     with open(output_file_name, "w") as output_file:
         print("angle,pixel,absorption,darkfield",
               file=output_file)
